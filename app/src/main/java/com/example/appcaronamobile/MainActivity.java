4eaf68970158ffca914968bcece68f5a853a4c0f
AlessSilva <alessandrosouza.eq@gmail.com>
2019-10-19 16:25:58 -0300

    Adição do Menu

--- app/src/main/java/com/example/appcaronamobile/MainActivity.java
+++ app/src/main/java/com/example/appcaronamobile/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.appcaronamobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.AddCarona:
+                Toast.makeText(this, "Adicionar Carona", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.HistoricoCaronas:
+                Toast.makeText(this, "Históricos de Caronas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.VerAmigos:
+                Toast.makeText(this, "Ver Amigos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.VerVeiculos:
+                Toast.makeText(this, "Ver Veículos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.AbrirChart:
+                Toast.makeText(this, "Abrir Chart", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Sobre:
+                Toast.makeText(this, "Sobre", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Configuracoes:
+                Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
