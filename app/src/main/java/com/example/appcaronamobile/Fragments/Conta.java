802338998b0f32ab547b1f11e9d5808dbc3253e1
Carlos <carlosferreirahd@gmail.com>
2019-10-23 13:10:38 -0300

    Fragmento Conta Finalizado

--- app/src/main/java/com/example/appcaronamobile/Fragments/Conta.java
+++ app/src/main/java/com/example/appcaronamobile/Fragments/Conta.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +23,7 @@ import com.example.appcaronamobile.Util.Masks.MaskEditUtil;
 public class Conta extends Fragment {
     Usuario usuario = null;
     private String senhaPedida = "";
+    private EditText input;
 
     private EditText nomeE;
     private EditText sobrenomeE;
@@ -58,6 +60,9 @@ public class Conta extends Fragment {
         nomeE = view.findViewById(R.id.editTextPrimeiroNomeConta);
         sobrenomeE = view.findViewById(R.id.editTextSobrenomeConta);
         telefoneE = view.findViewById(R.id.editTextTelefoneConta);
+
+        telefoneE.addTextChangedListener(MaskEditUtil.mask(telefoneE, MaskEditUtil.FORMAT_PHONE));
+
         emailE = view.findViewById(R.id.editTextEmailConta);
         senhaE = view.findViewById(R.id.editTextSenhaConta);
         senharepetidaE = view.findViewById(R.id.editTextSenhaRepetidaConta);
@@ -79,6 +84,11 @@ public class Conta extends Fragment {
             sitB = view.findViewById(R.id.radioButtonOutro);
         }
 
+        nomeE.setText(usuario.getPrimeiroNome());
+        sobrenomeE.setText(usuario.getSobrenome());
+        telefoneE.setText(usuario.getTelefone());
+        emailE.setText(usuario.getEmail());
+
         instB.setChecked(true);
         sitB.setChecked(true);
 
@@ -87,8 +97,8 @@ public class Conta extends Fragment {
         finalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                inst = instB.getText().toString();
-                sit = sitB.getText().toString();
+                inst = ((RadioButton) getView().findViewById(rginst.getCheckedRadioButtonId())).getText().toString();
+                sit = ((RadioButton) getView().findViewById(rgsit.getCheckedRadioButtonId())).getText().toString();
 
                 nome = nomeE.getText().toString();
                 sobrenome = sobrenomeE.getText().toString();
@@ -119,8 +129,8 @@ public class Conta extends Fragment {
 
                     AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                     builder.setTitle("Digite sua antiga senha");
-                    final EditText input = new EditText(getContext());
-                    input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    input = new EditText(getContext());
+                    input.setTransformationMethod(new PasswordTransformationMethod());
                     builder.setView(input);
 
                     builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -136,6 +146,8 @@ public class Conta extends Fragment {
                                 usuario.setSenha(senha);
                                 usuario.setSituacao(sit);
                                 usuario.setTelefone(telefone);
+
+                                Toast.makeText(getContext(), "Alterações Salvas", Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(getContext(), "Senha incorreta", Toast.LENGTH_SHORT).show();
                             }
@@ -156,81 +168,4 @@ public class Conta extends Fragment {
 
         return view;
     }
-
-//    protected void onClickFinalizarConta() {
-//        EditText nomeE = view.findViewById(R.id.editTextPrimeiroNomeConta);
-//        EditText sobrenomeE = view.findViewById(R.id.editTextSobrenomeConta);
-//        EditText telefoneE = view.findViewById(R.id.editTextTelefoneConta);
-//        EditText emailE = view.findViewById(R.id.editTextEmailConta);
-//        EditText senhaE = view.findViewById(R.id.editTextSenhaConta);
-//        EditText senharepetidaE = view.findViewById(R.id.editTextSenhaRepetidaConta);
-//
-//        RadioGroup rginst = view.findViewById(R.id.radioGroupInstituicoes);
-//        RadioGroup rgsit = view.findViewById(R.id.radioGroupSituacao);
-//
-//        inst = ((RadioButton) view.findViewById(rginst.getCheckedRadioButtonId())).getText().toString();
-//        sit = ((RadioButton) view.findViewById(rgsit.getCheckedRadioButtonId())).getText().toString();
-//
-//        nome = nomeE.getText().toString();
-//        sobrenome = sobrenomeE.getText().toString();
-//        telefone = telefoneE.getText().toString();
-//        email = emailE.getText().toString();
-//        senha = senhaE.getText().toString();
-//        senharepetida = senharepetidaE.getText().toString();
-//
-//        if(nome.equals("")) {
-//            nome = usuario.getPrimeiroNome();
-//        }
-//        if(sobrenome.equals("")) {
-//            sobrenome = usuario.getSobrenome();
-//        }
-//        if(telefone.equals("")) {
-//            telefone = usuario.getTelefone();
-//        }
-//        if(email.equals("")) {
-//            email = usuario.getEmail();
-//        }
-//
-//        if(!senha.equals(senharepetida)) {
-//            Toast.makeText(view.getContext(), "Senhas estão diferentes!", Toast.LENGTH_LONG).show();
-//        } else {
-//            if(senha.equals("")) {
-//                senha = usuario.getSenha();
-//            }
-//
-//            AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-//            builder.setTitle("Digite sua antiga senha");
-//            final EditText input = new EditText(view.getContext());
-//            input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-//            builder.setView(input);
-//
-//            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    senhaPedida = input.getText().toString();
-//
-//                    if(senhaPedida.equals(usuario.getSenha())) {
-//                        usuario.setPrimeiroNome(nome);
-//                        usuario.setSobrenome(sobrenome);
-//                        usuario.setEmail(email);
-//                        usuario.setInstituicao(inst);
-//                        usuario.setSenha(senha);
-//                        usuario.setSituacao(sit);
-//                        usuario.setTelefone(telefone);
-//                    } else {
-//                        Toast.makeText(getView().getContext(), "Senha incorreta", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//
-//            builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    dialog.cancel();
-//                }
-//            });
-//
-//            builder.show();
-//        }
-//    }
 }
